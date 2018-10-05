562585a1649799617f7c77de3b60ba2a5de14424
Ваше имя <@mail.ru>
2018-10-05 15:47:11 +0300

    :D

---
+++ WindowsFormsApplication7/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form   {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    double Сумма = Convert.ToDouble(textBox1.Text);
+                    double Разность = Convert.ToDouble(textBox2.Text);
+                    double Свет = (Сумма - Разность) / 2;
+                    double Лампа = Свет + Разность;
+                    textBox3.Text = String.Format("Стоимость светодиода: {0}; Стоимость энергосберегающей лампы: {1}", Свет, Лампа);
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!");
+                }
+            }
+    }
+}
